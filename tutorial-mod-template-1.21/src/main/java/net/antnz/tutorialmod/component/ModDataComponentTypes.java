0eb8036dd8d130280efecd201a64981ad9ecb701
AntND <antonlenz508@gmail.com>
2025-07-30 21:43:26 +0200

    Update

--- tutorial-mod-template-1.21/src/main/java/net/antnz/tutorialmod/component/ModDataComponentTypes.java
+++ tutorial-mod-template-1.21/src/main/java/net/antnz/tutorialmod/component/ModDataComponentTypes.java
@@ -15,14 +15,14 @@ public class ModDataComponentTypes {
 
     public static final ComponentType<BlockPos> COORDINATES = register("coordinates", blockPosBuilder -> blockPosBuilder.codec(BlockPos.CODEC));
 
-    private static <T>ComponentType<T> register(String name, UnaryOperator<ComponentType.Builder<T>> builderUnaryOperator){
+
+    private static <T>ComponentType<T> register(String name, UnaryOperator<ComponentType.Builder<T>> unaryOperator){
         return Registry.register(Registries.DATA_COMPONENT_TYPE, Identifier.of(TutorialMod.MOD_ID, name),
-                builderUnaryOperator.apply(ComponentType.builder()).build());
+                unaryOperator.apply(ComponentType.builder()).build());
     }
 
     public static void registerDataComponentTypes(){
-        TutorialMod.LOGGER.info("Registering Data Component Types for " + TutorialMod.MOD_ID);
+        TutorialMod.LOGGER.info("Registering Mod Data Component Types for " + TutorialMod.MOD_ID);
     }
-
 
 }
