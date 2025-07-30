d8650f67dfb31301c60a32645310f8db8543308b
AntND <antonlenz508@gmail.com>
2025-07-30 22:23:31 +0200

    Update

--- tutorial-mod-template-1.21/src/main/java/net/antnz/tutorialmod/item/ModItems.java
+++ tutorial-mod-template-1.21/src/main/java/net/antnz/tutorialmod/item/ModItems.java
@@ -73,10 +73,8 @@ public class ModItems {
             new ArmorItem(ModArmorMaterials.PINK_GARNET_ARMOR_MATERIAL, ArmorItem.Type.BOOTS, new Item.Settings()
                     .maxDamage(ArmorItem.Type.BOOTS.getMaxDamage(15))));
 
-
-    public static final Item PINK_GARNET_HORSE_ARMOR = registerItem("pink_garnet_horse_armor",
+    public static final Item PINK_GARNET_HORSE_ARMOR = registerItem("pink_garnet_horse_Armor",
             new AnimalArmorItem(ModArmorMaterials.PINK_GARNET_ARMOR_MATERIAL, AnimalArmorItem.Type.EQUESTRIAN, false, new Item.Settings().maxCount(1)));
-
 
 
     public static Item registerItem(String name, Item item){
